7ff7ee2e204b5ab5582e56214cc858cd47c6aed1
Arthur Golubev (AG) <mail@ArthurGolubev.com>
2025-08-09 15:10:38 +0300

    Add files via upload

---
+++ ag85clcd/ag85clcdsrc/ag85clcdnanosec/code/1/ag85clcdnanosec.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <typeinfo>
+
+#include "..//..//..//..//ag85cpf//libssrc//ag85cpfl//cpp2011//apfcol11//1//afclnt11.hpp" //AG19850316CppframebraryPartCorelibrary1UptoinnanosecondsTimerCpp2011
+
+int main(int argc, char * argv[]) {
+    
+    using namespace AG85::Cppframebrary::Corelibrary1::CppVersion2011;
+
+	try {
+
+        UptoinnanosecondsTimer commandLineCommandDurationUptoinnanosecondsTimer;
+
+        std::string commandToPerform;
+
+        for (unsigned int argNumber = 1; argNumber < argc; ++argNumber){
+            commandToPerform += argv[argNumber];
+            commandToPerform += " ";
+        }
+
+        commandLineCommandDurationUptoinnanosecondsTimer.Run();
+
+        int res = std::system(commandToPerform.c_str());
+
+        commandLineCommandDurationUptoinnanosecondsTimer.Pause();
+        
+        std::cout
+            << commandLineCommandDurationUptoinnanosecondsTimer.GetInnanosecondsInterval().count();
+
+        return res;
+
+    } catch (std::bad_alloc& exception) {
+        std::clog << "failed to allocate memory (" << exception.what() << ")" << "\n";
+    } catch (std::exception& exception) {
+        std::clog << "ecxeption: " << exception.what() << " ";
+        std::clog << "(cpp exception type - " << typeid(exception).name() << ")\n";
+    } catch (...) {
+        std::clog << "exception" << "\n";
+    }
+
+    return EXIT_FAILURE;
+
+}
+
+// Author: Arthur Golubev 1985 (ArthurGolubev1985)
